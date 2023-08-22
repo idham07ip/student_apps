ed8baacd2c7aa2fd343a58f53314d4d5b5918bc7
idham16ip <idham16iramap@gmai.com>
2023-08-22 15:16:22 +0700

    Update homePage

--- lib/view/student_main_page.dart
+++ lib/view/student_main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StudentMainPage extends StatefulWidget {
   const StudentMainPage({super.key});
@@ -14,9 +15,186 @@ class _StudentMainPageState extends State<StudentMainPage> {
       body: SafeArea(
         child: Stack(
           children: [
-            const Positioned.fill(
+            Positioned.fill(
               child: Column(
-                children: [],
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 52,
+                          width: 52,
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+
+                        //
+                        const SizedBox(
+                          width: 12,
+                        ),
+
+                        //
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Cucung Sukardi",
+                                style: GoogleFonts.poppins(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+
+                              const SizedBox(
+                                height: 4,
+                              ),
+
+                              //
+                              Text(
+                                "6th Semester",
+                                style: GoogleFonts.poppins(
+                                  fontStyle: FontStyle.italic,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+
+                        //NOTIFICATIONS
+                        Container(
+                          height: 54,
+                          width: 54,
+                          decoration: BoxDecoration(
+                            border: Border.all(),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: const Center(
+                            child: Badge(
+                              child: Icon(
+                                Icons.notifications,
+                              ),
+                              backgroundColor: Colors.greenAccent,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  //
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 16,
+                    ),
+                    child: const TextField(),
+                  ),
+
+                  //
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          //
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 16),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Next Class",
+                                  style: GoogleFonts.poppins(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                TextButton(
+                                  onPressed: () {},
+                                  child: const Text(
+                                    "See All",
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+
+                          //
+                          Container(
+                            height: 240,
+                            decoration: BoxDecoration(
+                              color: Colors.deepPurple[100],
+                              borderRadius: BorderRadius.circular(24),
+                            ),
+                          ),
+
+                          //
+                          //
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              top: 18,
+                              bottom: 8,
+                              left: 16,
+                              right: 16,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Events",
+                                  style: GoogleFonts.poppins(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                TextButton(
+                                  onPressed: () {},
+                                  child: const Text(
+                                    "See All",
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+
+                          //
+                          Column(
+                            children: List.generate(
+                              10,
+                              (index) => Container(
+                                margin: const EdgeInsets.symmetric(
+                                  horizontal: 4,
+                                  vertical: 2,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Colors.pink[50],
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      height: 84,
+                                      width: 72,
+                                      decoration: BoxDecoration(
+                                        color: Colors.blue,
+                                        borderRadius: BorderRadius.circular(16),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
 
@@ -84,7 +262,7 @@ class _StudentMainPageState extends State<StudentMainPage> {
 
                   //CHAT_BUTTON
                   Container(
-                    padding: EdgeInsets.all(8),
+                    padding: const EdgeInsets.all(8),
                     decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(16),
